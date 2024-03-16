9415bc6af9df84d5326ba6da8cf88c0706d53b3f
feridmzd <fmemmedzade60@gmail.com>
2024-03-16 09:26:37 -0700

    methods add

---
+++ Program.cs
@@ -0,0 +1,128 @@
+﻿namespace Consolemethods
+{
+    internal class Program
+    {
+
+
+        //public static float Hell(float a, float b, string ope)
+        //{
+
+
+        //    if (ope == "+")
+        //    {
+        //        return a + b;
+        //    }
+        //    else if (ope == "-")
+        //    {
+
+
+        //        return a - b;
+        //    }
+        //    else if (ope == "*")
+        //    {
+        //        return a * b;
+        //    }
+        //    else if (ope == "/")
+        //    {
+
+        //        return a / b;
+        //    }
+        //    return a;
+
+        //}
+
+        //static void Main(string[] args)
+        //{
+
+        //    Console.WriteLine(Hell(4, 8, "*"));
+        //}
+
+
+
+
+
+
+        //public static int DuzbucaqSahe(int a, int b)
+        //{
+        //    return a * b;
+        //}
+        //public static double Cevreninsahesi(int r)
+        //{
+        //    return Math.PI * r * r;
+
+        //}
+        //public static double Uvbucaqcevre(int a, int b, int c, int r)
+        //{
+        //    return ((a + b + c) / 2) * r;
+        //}
+
+        //public static int Paralipiped(int a, int b, int c)
+        //{
+        //    return (a * b + a * c + b * c) * 2;
+        //}
+        //static void Main()
+
+
+        //{
+
+        //    Console.WriteLine(DuzbucaqSahe(2, 5));
+        //    Console.WriteLine(Cevreninsahesi(2));
+        //    Console.WriteLine(Uvbucaqcevre(4, 6, 7, 3));
+        //    Console.WriteLine(Paralipiped(3, 7, 6));
+
+        //}
+        //}
+
+
+
+
+
+
+
+
+        //public static void Birinciherf(string hey)
+        //{
+
+        //    string[] soz = hey.Split(' ');
+        //    foreach (string sozler in soz)
+        //    {
+
+        //        if (!string.IsNullOrEmpty(sozler))
+        //        {
+        //            Console.Write(sozler[0] + " ");
+        //        }
+        //    }
+
+        //}
+
+        //public static void Main(string[] args)
+        //{
+        //    string ad = "Ferid  Memmedzade";
+        //    Birinciherf(ad);
+        //}
+
+
+
+
+
+
+
+
+
+
+
+
+        //static string Sozbirlesme(string ad)
+        //{
+        //    string[] adlar = ad.Split(" ");
+        //    string result = adlar[0] + adlar[1];
+        //    return result;
+        //}
+        //static void Main(string[] args)
+        //{
+            
+        //    Console.WriteLine(Sozbirlesme("Ferid Memmedzade"));
+        //}
+    }
+    }
+
